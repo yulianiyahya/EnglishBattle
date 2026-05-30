fccd32bee97b61db2c5e57bf2aee81ebb6604962
yulianiyahya <if24.yulianirahhayyu@mhs.ubpkarawang.ac.id>
2026-05-30 12:52:35 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'english-battle',
+  webDir: 'www'
+};
+
+export default config;
